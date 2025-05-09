a2401ed0a8c2bd90b6db3551ee1b8a5242d99d36
rahul <sabharwalonnet@gmail.com>
2025-05-09 08:27:13 +0300

    ok

--- designingmicroservices/services/commandorchestratorservice/commandorchestrationservice/src/main/java/com/example/postservice/repository/CommandRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.postservice.repository;
-
-import com.example.postservice.model.CommandRequest;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommandRequestRepository extends MongoRepository<CommandRequest, String> {
-}
