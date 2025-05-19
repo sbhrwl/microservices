01c1d8cc172402da2db15996378d80922c62914d
sbhrwl <sabharwalonnet@gmail.com>
2025-05-19 14:35:44 +0300

    ok

---
+++ sensorregistration/sensor-registration/ui-service/src/main/java/com/example/ui_service/UiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ui_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiServiceApplication.class, args);
+	}
+
+}
