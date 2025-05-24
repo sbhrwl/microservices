9b500791fa2e96ed837b94a6287a969e5d6767c0
rahul <sabharwalonnet@gmail.com>
2025-05-24 20:43:55 +0300

    ok

--- sensorregistration/sensor-registration/sensor-service/src/main/java/com/example/sensorservice/service/KafkaProducerService.java
+++ sensorregistration/sensor-registration/sensor-service/src/main/java/com/example/sensorservice/service/KafkaProducerService.java
@@ -1,12 +1,19 @@
 package com.example.sensorservice.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class KafkaProducerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducerService.class);
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
@@ -15,6 +22,8 @@ public class KafkaProducerService {
     private String sensorRegistrationTopic;
 
     public void sendSensorRegistrationMessage(String message) {
-        kafkaTemplate.send(sensorRegistrationTopic, message);
+        logger.info("Sending message to Kafka: {}", message);
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send(sensorRegistrationTopic, message);
+        future.thenAccept(result -> logger.info("Sent sensor registration message with sensorId: {}", message));
     }
 }
