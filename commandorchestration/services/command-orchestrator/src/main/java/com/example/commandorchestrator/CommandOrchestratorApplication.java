b6801f2645bead9474d8b15975c3bb276a2caabf
sbhrwl <sabharwalonnet@gmail.com>
2025-06-06 10:49:21 +0300

    ok

---
+++ commandorchestration/services/command-orchestrator/src/main/java/com/example/commandorchestrator/CommandOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.commandorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandOrchestratorApplication.class, args);
+	}
+
+}
---
+++ commandorchestration/services/command-orchestrator/src/test/java/com/example/commandorchestrator/CommandOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commandorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommandOrchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
