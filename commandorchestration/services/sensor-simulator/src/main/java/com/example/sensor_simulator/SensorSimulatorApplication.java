8a662ef44eb06194b27d8bfd0ddbabb274210701
sbhrwl <sabharwalonnet@gmail.com>
2025-06-06 14:19:31 +0300

    ok

---
+++ commandorchestration/services/sensor-simulator/src/main/java/com/example/sensor_simulator/SensorSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.sensor_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorSimulatorApplication.class, args);
+	}
+
+}
---
+++ commandorchestration/services/sensor-simulator/src/test/java/com/example/sensor_simulator/SensorSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sensor_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
