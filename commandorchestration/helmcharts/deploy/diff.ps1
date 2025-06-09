ae31ca48cc65606217cbf06979530cd0bfa830a8
sbhrwl <sabharwalonnet@gmail.com>
2025-06-09 10:54:55 +0300

    ok

--- commandorchestration/helmcharts/deploy/diff.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [string]$env = "dev"
-)
-
-$releaseName = "orchestration-release-$env"
-$valuesFile = "values-$env.yaml"
-
-Write-Host "Showing diff for Helm release: $releaseName..."
-helm diff upgrade $releaseName . -f $valuesFile
--- commandorchestration/helmcharts/deploy/uninstall-all.ps1
+++
@@ -1,9 +0,0 @@
-# Uninstall Helm releases
-helm uninstall orchestration-dev -n dev
-helm uninstall orchestration-staging -n staging
-helm uninstall orchestration-prod -n prod
-
-# Delete namespaces
-kubectl delete namespace dev
-kubectl delete namespace staging
-kubectl delete namespace prod
