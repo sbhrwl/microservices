d33e275095152dca5f7e6ca453eb6def292284c6
sbhrwl <sabharwalonnet@gmail.com>
2025-09-15 10:18:13 +0300

    ok

---
+++ hubToSensor/hub-to-sensor/flexibility-hub-simulator/src/main/java/com/apexsphere/flexibility_hub_simulator/FlexibilityHubSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.apexsphere.flexibility_hub_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexibilityHubSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexibilityHubSimulatorApplication.class, args);
+	}
+
+}
