31be8125494188027033cb78fbc3a335ea50630d
sbhrwl <sabharwalonnet@gmail.com>
2025-10-01 10:56:00 +0300

    ok

--- hubToSensor/hub-to-sensor/flexibility-bridge-service/src/main/java/com/apexsphere/flexibility_bridge_service/service/RecordGrpcClient.java
+++ hubToSensor/hub-to-sensor/flexibility-bridge-service/src/main/java/com/apexsphere/flexibility_bridge_service/service/RecordGrpcClient.java
@@ -3,37 +3,18 @@ package com.apexsphere.flexibility_bridge_service.service;
 import com.apexsphere.storage_service.service.RecordRequest;
 import com.apexsphere.storage_service.service.RecordResponse;
 import com.apexsphere.storage_service.service.RecordServiceGrpc;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PreDestroy;
 
 @Service
 public class RecordGrpcClient {
 
-    private final ManagedChannel channel;
     private final RecordServiceGrpc.RecordServiceBlockingStub blockingStub;
-
-    public RecordGrpcClient() {
-        // Build channel manually
-        this.channel = ManagedChannelBuilder
-                .forAddress("localhost", 9090)
-                .usePlaintext() // plaintext for dev
-                .build();
-
-        this.blockingStub = RecordServiceGrpc.newBlockingStub(channel);
+    public RecordGrpcClient(RecordServiceGrpc.RecordServiceBlockingStub blockingStub) {
+        this.blockingStub = blockingStub;
     }
 
     public String saveRecord(RecordRequest request) {
         RecordResponse response = blockingStub.saveRecord(request);
         return response.getMessage();
-    }
-
-    @PreDestroy
-    public void shutdown() {
-        if (channel != null && !channel.isShutdown()) {
-            channel.shutdown();
-        }
     }
 }
