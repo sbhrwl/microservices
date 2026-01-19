d1bb9a0f6f8834838391eff2e452980d7dc2ae65
sbhrwl <sabharwalonnet@gmail.com>
2026-01-19 15:43:12 +0200

    ok

---
+++ sensorregistration/graphql/backend/src/main/java/com/example/sensor/repository/SensorRepositoryException.java
@@ -0,0 +1,7 @@
+package com.example.sensor.repository;
+
+public class SensorRepositoryException extends RuntimeException {
+    public SensorRepositoryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
